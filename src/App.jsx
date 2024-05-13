7d30bbc6f48b2dfcbce49b0cb508a64b24f40a44
unknown <sajithroshitha98@gmail.com>
2024-05-13 23:27:26 +0530

    update filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState, useEffect
 
+ } from "react"
+import "./sass/main.scss"
+import { Users } from "./users"
 
 const App = () => {
+
+  const [query, setQuery] = useState("")
+
+  useEffect(() => {
+    console.log(query)
+  }, [query])
+
   return(
     <div className="app">
-      Hello REACT.JS
+      <div className="item-wrapper">
+        <input type="text"
+              placeholder="Search..."
+              className="search"
+              onChange={(e => setQuery(e.target.value))}/>
+        <ul className="list">
+          {Users.filter((user) =>
+            user.first_name.toLowerCase().includes(query)).map((user) => (
+            <li key={user.id} className="list-item">
+              {user.first_name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
